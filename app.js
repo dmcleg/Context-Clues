e674d7ca3f19a26dec7efcb7bf4c5ebb6e1efa96
dmcleg <dmcleghorn@gmail.com>
2021-09-30 13:57:25 -0500

    commit

---
+++ app.js
@@ -0,0 +1,46 @@
+for (var index = 1; index < 99; index++) {
+    var friends = ['Preston',
+        'IZA',
+        'Andy',
+        'JD',
+        'Hope']
+
+    var places = ['Unending Woods',
+        'The Tower',
+        'Dragon Tears',
+        'Upper Oceans',
+        'Bottom of Heaven',
+        'Caves',
+        'Garden of Steel',
+        'Third Place',
+        'Solar Valley',
+        'Halls of Creation']
+
+    var objects = ['boar jerky',
+        'bone ax',
+        'baseball bat',
+        'lilac raiment',
+        'speargun',
+        'kicks that rock the air',
+        'amethyst flames',
+        'garden steel sword',
+        'firecracker bat',
+        'quickcaster',
+        'Neb Djeser',
+        'Fugitive Hours',
+        'death laser',
+        'big bomb',
+        'Non',
+        'Rising Slash',
+        'hand drill',
+        'fireworks',
+        'Homerun Goddess',
+        "Philosopher's Needle"]
+
+    var h3 = document.createElement('h3')
+
+    h3.textContent = ("Accusation " + (index + 1));
+
+    body.append(h3);
+
+}
